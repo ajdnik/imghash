f75d531f82c7739b36a883f443cfac3b9f80091b
Rok Ajdnik <r.ajdnik@gmail.com>
2020-07-18 12:35:06 +0200

    fix: similarity package header & description

--- similarity/distance.go
+++ similarity/distance.go
@@ -1,3 +1,9 @@
+// Copyright 2020 Rok Ajdnik. All rights reserved.
+// Use of this source code is governed by the MIT
+// license that can be found in the LICENSE file.
+
+// Package similarity implements data types and methods used
+// to calculate similarities between hashes.
 package similarity
 
 import (
